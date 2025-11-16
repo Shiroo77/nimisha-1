ac93754d70465e359c4d0c624fcc07812b1959ce
Prem Ahankare <141759574+Shiroo77@users.noreply.github.com>
2025-11-16 16:54:00 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import GratitudePage from './components/GratitudePage';
+
+function App() {
+  return (
+    <div className="App">
+      <GratitudePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ types.ts
@@ -0,0 +1,1 @@
+// Type definitions have been removed as they are no longer needed for the new interaction.
